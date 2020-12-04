cff8e9fcaa341badafdc4ed2d0b9e7eac4b9339c
nyagz <44738554+nyagz@users.noreply.github.com>
2020-12-04 05:51:39 +0000

    Initial commit

---
+++ src/Buhlmann.java
@@ -0,0 +1,9 @@
+public class Buhlmann {
+
+    public static void main(String args[]){
+        System.out.println("I am actually starting to write code for my diss project!");
+        System.out.println("Imagine");
+        System.out.println("Who would've thought");
+        System.out.println("Not me");
+    }
+}
