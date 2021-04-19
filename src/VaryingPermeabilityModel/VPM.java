35b009a4144a49b0d66e741770fd426fb0794058
nyagz <44738554+nyagz@users.noreply.github.com>
2021-04-19 17:26:24 +0100

    VPM instructions

--- src/VaryingPermeabilityModel/VPM.java
+++ src/VaryingPermeabilityModel/VPM.java
@@ -8,6 +8,7 @@ public class VPM {
     public static final double crumblingCompression = 2.57;
     public static final double minimumInitialRadius = 0.8;
 
+    // TODO: descent and dive to be done the same way as Buhlmann's but ascent should use VPM instead of ZHL16
     public static void main(String[] args){
         System.out.println(planDive());
 
